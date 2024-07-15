5ed8f5c05224a14a291db92780870d353e2df4e2
daniil.prokofiev@mobius-software.com <daniil.prokofiev@mobius-software.com>
2024-07-15 15:37:46 +0200

    Adding session management to the Mb2c interface

---
+++ api/src/main/java/com/mobius/software/telco/protocols/diameter/app/mb2c/SessionFactory.java
@@ -0,0 +1,34 @@
+package com.mobius.software.telco.protocols.diameter.app.mb2c;
+/*
+ * Mobius Software LTD
+ * Copyright 2023, Mobius Software LTD and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+import com.mobius.software.telco.protocols.diameter.commands.mb2c.GCSActionRequest;
+import com.mobius.software.telco.protocols.diameter.commands.mb2c.GCSNotificationRequest;
+import com.mobius.software.telco.protocols.diameter.exceptions.AvpNotSupportedException;
+
+public interface SessionFactory
+{
+	public GCSActionClientSession createClientSession(GCSActionRequest request) throws AvpNotSupportedException;	
+	
+	public GCSActionServerSession createServerSession(GCSActionRequest request) throws AvpNotSupportedException;	
+	
+	public GCSNotificationClientSession createClientSession(GCSNotificationRequest request) throws AvpNotSupportedException;	
+	
+	public GCSNotificationServerSession createServerSession(GCSNotificationRequest request) throws AvpNotSupportedException;		
+}
