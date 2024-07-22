60411cf9a565404f7729d5386178622438f9c866
Yulian Oifa <yulian.oifa@mobius-software.com>
2024-07-22 17:50:30 +0200

    Fixing CC Server Session

--- api/src/main/java/com/mobius/software/telco/protocols/diameter/app/creditcontrol/CreditControlServerSession.java
+++ api/src/main/java/com/mobius/software/telco/protocols/diameter/app/creditcontrol/CreditControlServerSession.java
@@ -1,30 +1,11 @@
 package com.mobius.software.telco.protocols.diameter.app.creditcontrol;
 
-/*
- * Mobius Software LTD, Open Source Cloud Communications
- * Copyright 2023, Mobius Software LTD and individual contributors
- * by the @authors tag.
- *
- * This program is free software: you can redistribute it and/or modify
- * under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
-
-import com.mobius.software.telco.protocols.diameter.app.ServerAuthSession;
+import com.mobius.software.telco.protocols.diameter.app.ServerCCSession;
 import com.mobius.software.telco.protocols.diameter.commands.creditcontrol.AbortSessionRequest;
 import com.mobius.software.telco.protocols.diameter.commands.creditcontrol.CreditControlAnswer;
 import com.mobius.software.telco.protocols.diameter.commands.creditcontrol.ReAuthRequest;
 import com.mobius.software.telco.protocols.diameter.commands.creditcontrol.SessionTerminationAnswer;
 
-public interface CreditControlServerSession extends ServerAuthSession<CreditControlAnswer,ReAuthRequest,AbortSessionRequest,SessionTerminationAnswer>
+public interface CreditControlServerSession extends ServerCCSession<CreditControlAnswer,ReAuthRequest,AbortSessionRequest,SessionTerminationAnswer>
 {
 }
