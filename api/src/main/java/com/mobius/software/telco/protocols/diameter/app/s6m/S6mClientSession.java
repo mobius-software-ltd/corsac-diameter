ca909a11412a0e12bdab366503d47a067fb0c646
daniil.prokofiev@mobius-software.com <daniil.prokofiev@mobius-software.com>
2024-08-02 15:09:59 +0200

    Adding session management to S6m interface

---
+++ api/src/main/java/com/mobius/software/telco/protocols/diameter/app/s6m/S6mClientSession.java
@@ -0,0 +1,11 @@
+package com.mobius.software.telco.protocols.diameter.app.s6m;
+import com.mobius.software.telco.protocols.diameter.app.ClientAuthSessionStateless;
+import com.mobius.software.telco.protocols.diameter.commands.s6m.S6mRequest;
+/**
+*
+* @author yulian oifa
+*
+*/
+public interface S6mClientSession extends ClientAuthSessionStateless<S6mRequest>
+{
+}
---
+++ api/src/main/java/com/mobius/software/telco/protocols/diameter/app/s6m/S6mServerSession.java
@@ -0,0 +1,11 @@
+package com.mobius.software.telco.protocols.diameter.app.s6m;
+import com.mobius.software.telco.protocols.diameter.app.ServerAuthSessionStateless;
+import com.mobius.software.telco.protocols.diameter.commands.s6m.S6mAnswer;
+/**
+*
+* @author yulian oifa
+*
+*/
+public interface S6mServerSession extends ServerAuthSessionStateless<S6mAnswer>
+{
+}
