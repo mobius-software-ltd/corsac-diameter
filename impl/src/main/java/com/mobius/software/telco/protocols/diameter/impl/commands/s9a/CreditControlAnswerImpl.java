20bcce288b5cd47da21da2d449a57d6822df960f
daniil.prokofiev@mobius-software.com <daniil.prokofiev@mobius-software.com>
2024-09-20 09:51:52 +0200

    Fix CCA

--- impl/src/main/java/com/mobius/software/telco/protocols/diameter/impl/commands/s9a/CreditControlAnswerImpl.java
+++ impl/src/main/java/com/mobius/software/telco/protocols/diameter/impl/commands/s9a/CreditControlAnswerImpl.java
@@ -71,7 +71,7 @@ public class CreditControlAnswerImpl extends com.mobius.software.telco.protocols
 	{
 		super();
 		setExperimentalResultAllowed(true);
-		setOriginStateIdAllowedAllowed(true);
+		setOriginStateIdAllowed(true);
 		setUsernameAllowed(false);
 	}
 	
@@ -79,7 +79,7 @@ public class CreditControlAnswerImpl extends com.mobius.software.telco.protocols
 	{
 		super(originHost, originRealm, isRetransmit, resultCode, sessionID, authApplicationId, ccRequestType, ccRequestNumber);
 		setExperimentalResultAllowed(true);
-		setOriginStateIdAllowedAllowed(true);	
+		setOriginStateIdAllowed(true);	
 		setUsernameAllowed(false);
 	}
 	
