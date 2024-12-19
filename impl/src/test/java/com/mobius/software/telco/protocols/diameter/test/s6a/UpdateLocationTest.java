9833d8087d73c14c97e814ca70e0d0009b6d8c15
daniil.prokofiev@mobius-software.com <daniil.prokofiev@mobius-software.com>
2024-12-19 14:42:14 +0200

    Adding s6a tests

---
+++ impl/src/test/java/com/mobius/software/telco/protocols/diameter/test/s6a/UpdateLocationTest.java
@@ -0,0 +1,248 @@
+package com.mobius.software.telco.protocols.diameter.test.s6a;
+/*
+ * Mobius Software LTD
+ * Copyright 2019 - 2023, Mobius Software LTD and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.restcomm.cluster.ClusteredID;
+
+import com.mobius.software.telco.protocols.diameter.ApplicationIDs;
+import com.mobius.software.telco.protocols.diameter.AsyncCallback;
+import com.mobius.software.telco.protocols.diameter.DiameterLink;
+import com.mobius.software.telco.protocols.diameter.DiameterSession;
+import com.mobius.software.telco.protocols.diameter.DiameterStack;
+import com.mobius.software.telco.protocols.diameter.NetworkListener;
+import com.mobius.software.telco.protocols.diameter.ResultCodes;
+import com.mobius.software.telco.protocols.diameter.app.ClientAuthSessionStateless;
+import com.mobius.software.telco.protocols.diameter.app.ServerAuthSessionStateless;
+import com.mobius.software.telco.protocols.diameter.app.SessionStateEnum;
+import com.mobius.software.telco.protocols.diameter.app.s6a.ClientListener;
+import com.mobius.software.telco.protocols.diameter.app.s6a.S6aClientSession;
+import com.mobius.software.telco.protocols.diameter.app.s6a.ServerListener;
+import com.mobius.software.telco.protocols.diameter.commands.DiameterMessage;
+import com.mobius.software.telco.protocols.diameter.commands.DiameterRequest;
+import com.mobius.software.telco.protocols.diameter.commands.s6a.S6aAnswer;
+import com.mobius.software.telco.protocols.diameter.commands.s6a.S6aRequest;
+import com.mobius.software.telco.protocols.diameter.commands.s6a.UpdateLocationAnswer;
+import com.mobius.software.telco.protocols.diameter.commands.s6a.UpdateLocationRequest;
+import com.mobius.software.telco.protocols.diameter.exceptions.DiameterException;
+import com.mobius.software.telco.protocols.diameter.impl.app.s6a.S6aProviderImpl;
+import com.mobius.software.telco.protocols.diameter.primitives.gx.RATTypeEnum;
+import com.mobius.software.telco.protocols.diameter.primitives.s6a.ULRFlags;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+/**
+*
+* @author yulian oifa
+*
+*/
+public class UpdateLocationTest extends NetworkTestBase
+{
+	protected static final String localListenerID = "1";
+	private static final Logger logger = LogManager.getLogger(UpdateLocationTest.class);
+	
+	@Test
+	public void testAuthenticationInformation() throws Exception
+	{
+		super.setupRemote(0L,0L);
+		super.setupLocal(0L,0L);
+		
+		final AtomicLong ulaReceived=new AtomicLong(0L);
+		final AtomicLong ulaReceivedByListener=new AtomicLong(0L);
+		final AtomicLong ulrReceived=new AtomicLong(0L);
+		final AtomicLong ulrReceivedByListener=new AtomicLong(0L);
+		final AtomicLong timeoutReceived=new AtomicLong(0L);
+		
+		DiameterStack localStack = this.localStack;
+		DiameterStack serverStack = this.serverStack;
+		
+		localStack.getNetworkManager().addNetworkListener(localListenerID, new NetworkListener()
+		{
+			@Override
+			public void onMessage(DiameterMessage message, String linkID, AsyncCallback callback)
+			{
+				if(message instanceof S6aAnswer)
+					ulrReceived.incrementAndGet();				
+			}
+		});
+		
+		serverStack.getNetworkManager().addNetworkListener(localListenerID, new NetworkListener()
+		{
+			@Override
+			public void onMessage(DiameterMessage message, String linkID, AsyncCallback callback)
+			{
+				if(message instanceof S6aRequest)
+					ulaReceived.incrementAndGet();				
+			}
+		});
+		
+		try
+		{
+			Thread.sleep(reconnectTimeout * 2);
+		}
+		catch(InterruptedException ex)
+		{
+			
+		}
+		
+		S6aProviderImpl provider = (S6aProviderImpl)localStack.getProvider(Long.valueOf(ApplicationIDs.S6A), Package.getPackage("com.mobius.software.telco.protocols.diameter.commands.s6a"));
+		ClusteredID<?> listenerID=generator.generateID();
+		provider.setClientListener(listenerID, new ClientListener()
+		{
+			@Override
+			public void onTimeout(DiameterRequest request,DiameterSession session)
+			{
+				timeoutReceived.incrementAndGet();
+			}
+			
+			@Override
+			public void onIdleTimeout(DiameterSession session)
+			{
+				timeoutReceived.incrementAndGet();
+			}
+
+			@Override
+			public void onInitialAnswer(S6aAnswer answer, ClientAuthSessionStateless<S6aRequest> session, String linkID,AsyncCallback callback)
+			{
+				ulaReceivedByListener.incrementAndGet();
+			}
+			
+		});
+		
+		S6aProviderImpl serverProvider = (S6aProviderImpl)serverStack.getProvider(Long.valueOf(ApplicationIDs.S6A), Package.getPackage("com.mobius.software.telco.protocols.diameter.commands.s6a"));
+		serverProvider.setServerListener(listenerID, new ServerListener()
+		{
+			@Override
+			public void onTimeout(DiameterRequest request,DiameterSession session)
+			{
+				timeoutReceived.incrementAndGet();
+			}
+			
+			@Override
+			public void onIdleTimeout(DiameterSession session)
+			{
+				timeoutReceived.incrementAndGet();
+			}
+
+			@Override
+			public void onInitialRequest(S6aRequest request, ServerAuthSessionStateless<S6aAnswer> session,String linkID, AsyncCallback callback)
+			{
+				ulrReceivedByListener.incrementAndGet();
+				UpdateLocationRequest ulr=(UpdateLocationRequest)request;
+				try
+				{
+					UpdateLocationAnswer aia = serverProvider.getMessageFactory().createUpdateLocationAnswer(ulr, ulr.getHopByHopIdentifier(), ulr.getEndToEndIdentifier(), ResultCodes.DIAMETER_SUCCESS);
+					session.sendInitialAnswer(aia, new AsyncCallback()
+					{
+						@Override
+						public void onSuccess()
+						{
+							
+						}
+						
+						@Override
+						public void onError(DiameterException ex)
+						{
+							logger.error("An error occured while sending Update Location Answer," + ex.getMessage(),ex);
+						}
+					});
+				}
+				catch(DiameterException ex)
+				{
+					logger.error("An error occured while sending Update Location Answer," + ex.getMessage(),ex);
+				}
+			}
+			
+		});
+		
+		//usually its not needed to get link, here we use it to read hosts/realms
+		DiameterLink localLink = localStack.getNetworkManager().getLink(localLinkID);
+		
+		byte[] data=new byte[] { 0x01,0x02,0x03};
+		ByteBuf visitedPLMNId = Unpooled.wrappedBuffer(data);
+		
+		ULRFlags ulrFlags = provider.getAvpFactory().getULRFlags();
+		ulrFlags.setDualRegistration5GIndicationBit(true);;
+		
+		
+		UpdateLocationRequest request = provider.getMessageFactory().createUpdateLocationRequest(localLink.getLocalHost(), localLink.getLocalRealm(), localLink.getDestinationHost(), localLink.getDestinationRealm(),RATTypeEnum.CDMA2000_1X,ulrFlags,visitedPLMNId);
+		
+		S6aClientSession clientSession = (S6aClientSession)provider.getSessionFactory().createClientSession(request);
+		
+		clientSession.sendInitialRequest(request, new AsyncCallback()
+		{
+			@Override
+			public void onSuccess()
+			{
+			}
+			
+			@Override
+			public void onError(DiameterException ex)
+			{
+			}
+		});
+		
+		try
+		{
+			Thread.sleep(responseTimeout);
+		}
+		catch(InterruptedException ex)
+		{
+			
+		}
+		
+		assertEquals(clientSession.getSessionState(),SessionStateEnum.IDLE);
+		
+		//make sure no timeout is processed
+		try
+		{
+			Thread.sleep(idleTimeout * 2);
+		}
+		catch(InterruptedException ex)
+		{
+			
+		}
+		
+		super.stopLocal();
+		super.stopRemote();
+		
+		try
+		{
+			Thread.sleep(responseTimeout);
+		}
+		catch(InterruptedException ex)
+		{
+			
+		}
+		
+		assertEquals(ulaReceived.get() , 1L);
+		assertEquals(ulaReceivedByListener.get() , 1L);
+		assertEquals(ulrReceived.get() , 1L);
+		assertEquals(ulrReceivedByListener.get() , 1L);
+		assertEquals(timeoutReceived.get() , 0L);
+	}
+	
+}
